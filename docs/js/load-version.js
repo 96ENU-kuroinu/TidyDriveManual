a35c20202a1255139d86fcd3cf07c79f8f71b0af
96ENU <96enu.illustration@gmail.com>
2025-01-14 23:40:48 +0900

    1.2.2仕様に更新しました。

--- docs/js/load-version.js
+++ docs/js/load-version.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const versionElements = document.querySelectorAll('#version');
     if (versionElements) {
         versionElements.forEach((versionElement) => {
-            versionElement.textContent = "Ver 1.2.1";
+            versionElement.textContent = "Ver 1.2.2";
         })
     }
 });
