660ad265d7f0cdcb018be2aae9d1bfc204314847
96ENU <96enu.illustration@gmail.com>
2025-04-15 05:01:51 +0900

    マニュアル整備中…

--- docs/js/generate-text-by-image.js
+++ docs/js/generate-text-by-image.js
@@ -1,4 +1,6 @@
-// Function to update the text content for all span elements based on their data-image attribute
+/**
+ * data-image属性に基づいてすべてのspan要素のテキスト内容を更新する関数
+ */
 function updateSpanText() {
     // Select all span elements with the class 'hover-text'
     const spanElements = document.querySelectorAll('.hover-text');
@@ -11,20 +13,14 @@ function updateSpanText() {
         // Conditional logic to change text based on the data-image value
         let newText = '';
         switch (dataImage) {
-            case 'images/display-server-select.png':
-                newText = 'Misskeyサーバー選択画面';
+            case 'images/display-select-server.png':
+                newText = 'サーバー一覧画面';
+                break;
+            case 'images/display-select-alternative-server.png':
+                newText = 'サーバー一覧に掲載されていないサーバーの選択用ウィンドウ';
                 break;
             case 'images/display-setting.png':
                 newText = '設定画面';
-                break;
-            case 'images/display-playlist-detail.png':
-                newText = 'プレイリスト詳細画面';
-                break;
-            case 'images/display-playlists-2.png':
-                newText = 'プレイリスト一覧画面';
-                break;
-            case 'images/display-playlists-1.png':
-                newText = 'プレイリスト一覧画面';
                 break;
             case 'images/display-main.png':
                 newText = 'ホーム画面';
@@ -32,17 +28,11 @@ function updateSpanText() {
             case 'images/display-drawer.png':
                 newText = 'ドロワー';
                 break;
-            case 'images/display-channel-select-2.png':
-                newText = '投稿先チャンネル検索画面';
-                break;
-            case 'images/display-channel-select-1.png':
-                newText = '投稿先チャンネル検索画面';
-                break;
             case 'images/display-app-about.png':
                 newText = 'アプリについての画面';
                 break;
-            case 'images/display-sample-mfm.png':
-                newText = '作例画面';
+            case 'images/display-tag-manage.png':
+                newText = 'タグ管理画面';
                 break;
         }
 
